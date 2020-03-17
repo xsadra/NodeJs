eac465f23682fefccc896b9b7890f087a5db7390
Sadra <31988724+xsadra@users.noreply.github.com>
2020-03-17 14:24:22 +0100

    using 'validator' package to validate Email and URL -> [notes-app]

--- 03-notes-app/app.js
+++ 03-notes-app/app.js
@@ -1,5 +1,9 @@
+const validator = require('validator')
 const getNotes = require('./note.js')
 
 const notes = getNotes()
 
 console.log(notes);
+
+console.log(validator.isEmail('info@sadra.at'))
+console.log(validator.isURL('sadra.at'))
