ce5d550bea3fd0e6d0f22c26d4e4a62043f474e8
Sadra <31988724+xsadra@users.noreply.github.com>
2020-03-17 13:07:41 +0100

    add more lines to the text file using 'appendFileSync'

--- 01-notes-app/app.js
+++ 01-notes-app/app.js
@@ -1,3 +1,7 @@
 const  fs = require('fs')
 
-fs.writeFileSync('note.txt', 'New text file created by FS.')
+// fs.writeFileSync('note.txt', 'New text file created by FS.')
+
+/// uasing append to add text to existed file
+
+fs.appendFileSync('note.txt', 'add more line to the file.\n')
