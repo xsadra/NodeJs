32cba60c631600580a7cea76f35c4c889b3aee26
Sadra <31988724+xsadra@users.noreply.github.com>
2020-03-17 14:38:55 +0100

    add and using 'chalk' package to 'Terminal string styling' -> [notes-app]

--- 03-notes-app/app.js
+++ 03-notes-app/app.js
@@ -1,9 +1,12 @@
-const validator = require('validator')
+const chalk = require('chalk');
+// const validator = require('validator')
 const getNotes = require('./note.js')
 
 const notes = getNotes()
 
 console.log(notes);
 
-console.log(validator.isEmail('info@sadra.at'))
-console.log(validator.isURL('sadra.at'))
+// console.log(validator.isEmail('info@sadra.at'))
+// console.log(validator.isURL('sadra.at'))
+ 
+console.log(chalk.green('Hello world!'));
