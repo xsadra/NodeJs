674d125cd2176bf2adfde3875ee22e8ab85fdf20
Sadra <31988724+xsadra@users.noreply.github.com>
2020-03-20 20:45:51 +0100

    Using Command line Argument to Get data from user -> [notes-app]

---
+++ 04-notes-app/app.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+const getNotes = require('./note.js')
+
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding note!')
+    
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
