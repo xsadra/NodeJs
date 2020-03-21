0d08bc960cd42df6fb6a1042e778458a424faf20
Sadra <31988724+xsadra@users.noreply.github.com>
2020-03-21 20:31:06 +0100

    create 'loadNotes' method to load notes by 'note.js' and craete 'addNote' method -> [notes-app]

--- 04-notes-app/note.js
+++ 04-notes-app/note.js
@@ -1,5 +1,24 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your Notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+    const notes = loadNotes()
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffre = fs.readFileSync('notes.json')    
+        const dataJSON = dataBuffre.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
